da6a2cffdd0b01474a3ff697e13d6dba4a645ea8
IzzyBedic <izzy@bedichek.org>
2026-06-08 15:43:39 -0700

    Gitignore

--- terraform-fargate/outputs.tf
+++ terraform-fargate/outputs.tf
@@ -1,3 +1,16 @@
 output "alb_hostname" {
   value = "${aws_alb.main.dns_name}:3000"
 }
+
+# Consumed by the deploy workflow to push the image and roll out the services.
+output "cluster_name" {
+  value = aws_ecs_cluster.main.name
+}
+
+output "api_service_name" {
+  value = aws_ecs_service.main.name
+}
+
+output "worker_service_name" {
+  value = aws_ecs_service.worker.name
+}
